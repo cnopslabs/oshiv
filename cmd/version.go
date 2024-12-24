57d326d773acdd0b2846c1975d623917770cedc5
Volodymyr Rozdolsky <volodymyr.rozdolsky@oracle.com>
2024-12-24 15:24:18 -0500

    Add support for short (-v) and long (version) version commands

--- cmd/version.go
+++ cmd/version.go
@@ -8,16 +8,32 @@ import (
 // This will be populated dynamically at build time
 var version = "unknown"
 
-// Version command
+// Version command for long form usage
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the version number of oshiv cli",
-	Long:  `Print the version number of oshiv cli`,
+	Short: "Print the version number of oshiv CLI",
+	Long:  `Print the version number of oshiv CLI`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("oshiv version: %s\n", version)
+		printVersion()
 	},
 }
 
+// Helper function for printing the version
+func printVersion() {
+	fmt.Printf("oshiv version: %s\n", version)
+}
+
 func init() {
+	// Add the version command for long form (e.g., `oshiv version`)
 	rootCmd.AddCommand(versionCmd)
+	rootCmd.PersistentFlags().BoolP("version", "v", false, "Print the version number of oshiv CLI")
+	rootCmd.Flags().BoolP("version", "v", false, "Print the short version number of oshiv CLI")
+
+	cobra.OnInitialize(checkVersionFlag)
+}
+
+func checkVersionFlag() {
+	if versionFlag, _ := rootCmd.Flags().GetBool("version"); versionFlag {
+		printVersion()
+	}
 }
